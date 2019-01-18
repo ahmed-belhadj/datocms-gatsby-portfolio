3a3b19e80db73cb403b112be68d5b3abec1e63f9
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-01-17 22:26:26 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,8 @@ const IndexPage = ({ data }) => (
               <p>{work.excerpt}</p>
             </div>
           </figcaption>
+          <button/>
+          <button/>
         </figure>
       </div>
     ))}
